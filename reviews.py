d26ad18c3a2bf67f8516d93852c29ea6fb4f8ca6
Wayne <a0918964526@gmail.com>
2020-09-07 18:37:10 +0800

    upload version

--- reviews.py
+++ reviews.py
@@ -7,7 +7,11 @@ with open('reviews.txt', 'r') as f:
 		if count % 10000 ==0:
 			print(len(data))
 print('總共', len(data), '筆')
-
+length = 0
+for word in data:
+	length = length + len(word)
+average_length = length / len(data)	
+print('平均長度為： ', average_length)	
 
 
 
