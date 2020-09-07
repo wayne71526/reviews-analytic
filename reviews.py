c5d970e3dd5da977387ab9904e9a9a2e912c36f3
Wayne <a0918964526@gmail.com>
2020-09-08 01:11:56 +0800

    upload new version

--- reviews.py
+++ reviews.py
@@ -7,11 +7,27 @@ with open('reviews.txt', 'r') as f:
 		if count % 10000 ==0:
 			print(len(data))
 print('總共', len(data), '筆')
+
 length = 0
 for word in data:
 	length = length + len(word)
 average_length = length / len(data)	
-print('平均長度為： ', average_length)	
+print('平均長度為： ', average_length)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print(len(new))
+print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print(len(good))
+print(good[0])		
+
 
 
 
